4d88bbcb1dfbedd1dccdf5ce73150faeea68d9a3
Raychengole97 <raychengole97@gmail.com>
2020-08-28 10:22:57 +0300

    modify the viewadapter for the project

--- app/src/main/java/com/moringa/class_schedule_app/Adapters/ViewAdapter.java
+++ app/src/main/java/com/moringa/class_schedule_app/Adapters/ViewAdapter.java
@@ -1,4 +1,47 @@
-package com.moringa.class_schedule_app.Adapters;
+package com.example.back.Adapters;
 
-public class ViewAdapter {
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAdapter extends FragmentPagerAdapter {
+    ArrayList<String> arrayList=new ArrayList<>();
+    List<Fragment> fragmentList=new ArrayList<>();
+
+    public void addFragment(Fragment fragment,String title){
+        arrayList.add(title);
+        fragmentList.add(fragment);
+    }
+
+
+    public ViewAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        return fragmentList.get(position);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return arrayList.get(position);
+    }
 }
