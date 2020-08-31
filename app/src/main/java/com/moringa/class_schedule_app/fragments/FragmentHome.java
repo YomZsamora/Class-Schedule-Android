268a0588d2f96e12897d4fb5bf6bc01a8fa6cd4d
lloydM <lloydMwiti4@gmail.com>
2020-08-31 18:59:46 +0300

    creates a model for adding data to the listview in the home fragment

--- app/src/main/java/com/moringa/class_schedule_app/fragments/FragmentHome.java
+++ app/src/main/java/com/moringa/class_schedule_app/fragments/FragmentHome.java
@@ -5,19 +5,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.moringa.class_schedule_app.R;
+import com.moringa.class_schedule_app.fireModel.SetData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FragmentHome extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.fragment_home,container,false);
-        return v;
+        View root=inflater.inflate(R.layout.fragment_home,container,false);
+
+        ListView mlist=root.findViewById(R.id.mylist);
+        List<SetData> setData;
+        setData=new ArrayList<>();
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        setData.add(new SetData(R.drawable.pimg,"james","james@gmail.com"));
+        ListAdapter listAdapter=new ListAdapter(getContext(),R.layout.list_item,setData);
+        mlist.setAdapter(listAdapter);
+        return root;
     }
 }
