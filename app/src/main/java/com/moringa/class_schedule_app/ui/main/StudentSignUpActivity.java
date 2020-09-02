eadda54124ad5d3e052bac6d7d47d7a74de94997
Gideon Okuro <gideonollonde@gmail.com>
2020-09-02 16:09:15 +0300

    add a student signup form

--- app/src/main/java/com/moringa/class_schedule_app/ui/main/StudentSignUpActivity.java
+++ app/src/main/java/com/moringa/class_schedule_app/ui/main/StudentSignUpActivity.java
@@ -3,14 +3,36 @@ package com.moringa.class_schedule_app.ui.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.moringa.class_schedule_app.R;
 
-public class StudentSignUpActivity extends AppCompatActivity {
+public class StudentSignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_sign_up);
+
+        Spinner spinner = findViewById(R.id.cohort_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cohorts, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter((adapter));
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
