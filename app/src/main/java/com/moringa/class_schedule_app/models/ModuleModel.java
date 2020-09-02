c8e206455f2ad94977d7b8d861b35736aed1a9b2
Ben Ruwel <benruwel21@gmail.com>
2020-09-02 12:13:28 +0300

    Generate POJO code for ModuleModel class

---
+++ app/src/main/java/com/moringa/class_schedule_app/models/ModuleModel.java
@@ -0,0 +1,47 @@
+package com.moringa.class_schedule_app.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ModuleModel {
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public ModuleModel() {
+    }
+
+    /**
+     *
+     * @param name
+     * @param id
+     */
+    public ModuleModel(String name, Integer id) {
+        super();
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
