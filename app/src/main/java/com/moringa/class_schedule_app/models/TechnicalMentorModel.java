9863132188843bcb35d86efd1eaaf8f2397a6ab1
Ben Ruwel <benruwel21@gmail.com>
2020-09-02 11:54:25 +0300

    Generate POJO for TechnicalMentorModel class

---
+++ app/src/main/java/com/moringa/class_schedule_app/models/TechnicalMentorModel.java
@@ -0,0 +1,65 @@
+package com.moringa.class_schedule_app.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import org.parceler.Parcel;
+
+@Parcel
+public class TechnicalMentorModel {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("uid")
+    @Expose
+    private String uid;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public TechnicalMentorModel() {
+    }
+
+    /**
+     *
+     * @param uid
+     * @param name
+     * @param id
+     */
+    public TechnicalMentorModel(String name, String uid, Integer id) {
+        super();
+        this.name = name;
+        this.uid = uid;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
