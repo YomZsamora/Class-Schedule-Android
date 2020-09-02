133978e629fdaf2b1272420bec2c5578b9b7ac55
Ben Ruwel <benruwel21@gmail.com>
2020-09-02 12:18:33 +0300

    Generate POJO code for ModulesApiResponse class

---
+++ app/src/main/java/com/moringa/class_schedule_app/models/ModulesApiResponse.java
@@ -0,0 +1,30 @@
+package com.moringa.class_schedule_app.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+
+@Parcel
+public class ModulesApiResponse {
+    @SerializedName("students")
+    @Expose
+    private ArrayList<ModuleModel> modules = null;
+
+    public ModulesApiResponse() {
+    }
+
+    public ModulesApiResponse(ArrayList<ModuleModel> modules) {
+        this.modules = modules;
+    }
+
+    public ArrayList<ModuleModel> getModules() {
+        return modules;
+    }
+
+    public void setModules(ArrayList<ModuleModel> modules) {
+        this.modules = modules;
+    }
+}
