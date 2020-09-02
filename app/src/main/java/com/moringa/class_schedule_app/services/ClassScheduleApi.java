6f0c2bd92d03ea7141fa58c472728c2d780af3b3
Ben Ruwel <benruwel21@gmail.com>
2020-09-02 12:07:59 +0300

    Improve http reqs to Student, Technical Mentor and Sessions api endpoints

--- app/src/main/java/com/moringa/class_schedule_app/services/ClassScheduleApi.java
+++ app/src/main/java/com/moringa/class_schedule_app/services/ClassScheduleApi.java
@@ -2,26 +2,46 @@ package com.moringa.class_schedule_app.services;
 
 import com.moringa.class_schedule_app.models.SessionsApiResponse;
 import com.moringa.class_schedule_app.models.SessionsModel;
+import com.moringa.class_schedule_app.models.StudentModel;
+import com.moringa.class_schedule_app.models.StudentsApiResponse;
+import com.moringa.class_schedule_app.models.TechnicalMentorApiResponse;
+import com.moringa.class_schedule_app.models.TechnicalMentorModel;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ClassScheduleApi {
     //SESSIONS TABLE
+    //get a list of sessions
     @GET("sessions")
     Call<SessionsApiResponse> getSessionList();
-
+    //create new session
     @POST("session/new")
-    Call<SessionsModel> createNewSession();
+    Call<SessionsModel> createNewSession(
+            @Body SessionsModel session
+    );
 
+    //STUDENTS
+    //create student account
+    @POST("student/new")
+    Call<StudentModel> createStudent(
+            @Body StudentModel student
+    );
+    //get list of students
+    @GET("students")
+    Call<StudentsApiResponse> getStudentsList();
 
-   @POST("student/new")
-    Call<StudentModel> createStudent();
-
+    //TECHNICAL MENTORS
+    //create technical mentor account
     @POST("mentor/new")
-    Call<TechnicalMentorModel> createTechnicalMentor();
+    Call<TechnicalMentorModel> createTechnicalMentor(
+            @Body TechnicalMentorModel technicalMentor
+    );
+    @GET("mentors")
+    Call<TechnicalMentorApiResponse> getTechnicalMentorsList();
 }
 
 
