580ea40cf293078d1ee1cfea89fc911260e324b1
Ben Ruwel <benruwel21@gmail.com>
2020-09-03 11:28:31 +0300

    Complete design of session details activity

---
+++ app/src/main/java/com/moringa/class_schedule_app/ui/SessionDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.moringa.class_schedule_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.moringa.class_schedule_app.R;
+
+public class SessionDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_details);
+    }
+}
