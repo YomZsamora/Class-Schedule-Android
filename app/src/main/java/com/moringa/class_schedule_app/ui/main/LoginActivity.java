2b05f2295b0cba7c46336887b943b672dbcc083a
Gideon Okuro <gideonollonde@gmail.com>
2020-09-03 07:53:54 +0300

    add click listeners

--- app/src/main/java/com/moringa/class_schedule_app/ui/main/LoginActivity.java
+++ app/src/main/java/com/moringa/class_schedule_app/ui/main/LoginActivity.java
@@ -2,17 +2,46 @@ package com.moringa.class_schedule_app.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.moringa.class_schedule_app.R;
+import com.moringa.class_schedule_app.ui.MainActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
+    @BindView(R.id.LoginButton)
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, AccountTypeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        if (view == mLoginButton) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
