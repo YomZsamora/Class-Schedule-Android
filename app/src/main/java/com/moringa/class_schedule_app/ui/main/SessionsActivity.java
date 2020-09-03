2282be9fe56a07c5d11a778b4d401b2b365f4f99
Gideon Okuro <gideonollonde@gmail.com>
2020-09-03 14:25:46 +0300

    create interface to extend view on click listener

--- app/src/main/java/com/moringa/class_schedule_app/ui/main/SessionsActivity.java
+++ app/src/main/java/com/moringa/class_schedule_app/ui/main/SessionsActivity.java
@@ -2,16 +2,24 @@ package com.moringa.class_schedule_app.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.moringa.class_schedule_app.R;
+import com.moringa.class_schedule_app.ui.MainActivity;
 
-public class SessionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SessionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, SessionsActivityInterface {
+    @BindView(R.id.submitButton) Button mSubmitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,9 @@ public class SessionsActivity extends AppCompatActivity implements AdapterView.O
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter((adapter));
         spinner.setOnItemSelectedListener(this);
+
+        ButterKnife.bind(this);
+        mSubmitButton.setOnClickListener((View.OnClickListener) this);
     }
 
     @Override
@@ -33,6 +44,17 @@ public class SessionsActivity extends AppCompatActivity implements AdapterView.O
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mSubmitButton) {
+            Intent intent = new Intent(SessionsActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
 
     }
 }
---
+++ app/src/main/java/com/moringa/class_schedule_app/ui/main/SessionsActivityInterface.java
@@ -0,0 +1,6 @@
+package com.moringa.class_schedule_app.ui.main;
+
+import android.view.View;
+
+public interface SessionsActivityInterface extends View.OnClickListener {
+}
