bb748ea78c1027832dbc1be0d3aba3dac3c94bde
Gideon Okuro <gideonollonde@gmail.com>
2020-09-03 14:35:04 +0300

    bind layout

--- app/src/main/java/com/moringa/class_schedule_app/ui/main/SessionsActivity.java
+++ app/src/main/java/com/moringa/class_schedule_app/ui/main/SessionsActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,6 +20,14 @@ import butterknife.ButterKnife;
 
 public class SessionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, SessionsActivityInterface {
     @BindView(R.id.submitButton) Button mSubmitButton;
+    @BindView(R.id.editTextSessionName) EditText mEditTextSessionName;
+    @BindView(R.id.editTextStartTime) EditText mEditTextStartTime;
+    @BindView(R.id.editTextEndTime) EditText mEditTextEndTime;
+    @BindView(R.id.editTextDate) EditText mEditTextDate;
+    @BindView(R.id.cohort_spinner) Spinner mCohortSpinner;
+    @BindView(R.id.editTextModule) EditText mEditTextModule;
+    @BindView(R.id.editTextDescription) EditText mEditTextDescription;
+
 
 
     @Override
@@ -34,6 +43,12 @@ public class SessionsActivity extends AppCompatActivity implements AdapterView.O
 
         ButterKnife.bind(this);
         mSubmitButton.setOnClickListener((View.OnClickListener) this);
+        mEditTextSessionName.setOnClickListener((View.OnClickListener) this);
+        mEditTextStartTime.setOnClickListener((View.OnClickListener) this);
+        mEditTextEndTime.setOnClickListener((View.OnClickListener) this);
+        mEditTextDate.setOnClickListener((View.OnClickListener) this);
+        mEditTextModule.setOnClickListener((View.OnClickListener) this);
+        mEditTextDescription.setOnClickListener((View.OnClickListener) this);
     }
 
     @Override
