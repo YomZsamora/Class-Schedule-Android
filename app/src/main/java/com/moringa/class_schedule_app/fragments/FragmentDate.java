cb71c1029a86b9cc803b1d39484145dad4d20c1c
Gideon Okuro <gideonollonde@gmail.com>
2020-09-04 14:10:52 +0300

    add a oncreate dialog

--- app/src/main/java/com/moringa/class_schedule_app/fragments/FragmentDate.java
+++ app/src/main/java/com/moringa/class_schedule_app/fragments/FragmentDate.java
@@ -1,7 +1,12 @@
 package com.moringa.class_schedule_app.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,57 +15,20 @@ import android.view.ViewGroup;
 
 import com.moringa.class_schedule_app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentDate#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FragmentDate extends Fragment {
+import java.util.Calendar;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class FragmentDate extends DialogFragment {
+    public class dateFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentDate() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentDate.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentDate newInstance(String param1, String param2) {
-        FragmentDate fragment = new FragmentDate();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date, container, false);
-    }
 }
