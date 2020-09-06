2e7dca1f5eb07f4f9b7f3772e7cb35f909b9f04a
Ben Ruwel <benruwel21@gmail.com>
2020-09-06 14:21:10 +0300

    Fix bug in setting start and end time

--- app/src/main/java/com/moringa/class_schedule_app/ui/CreateSessionActivity.java
+++ app/src/main/java/com/moringa/class_schedule_app/ui/CreateSessionActivity.java
@@ -2,9 +2,12 @@ package com.moringa.class_schedule_app.ui;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +18,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
@@ -23,12 +27,14 @@ import com.moringa.class_schedule_app.fragments.FragmentDate;
 import com.moringa.class_schedule_app.fragments.TimeFragment;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class CreateSessionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+public class CreateSessionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener, DatePickerDialog.OnDateSetListener {
     @BindView(R.id.submitButton) Button mSubmitButton;
     @BindView(R.id.editTextSessionName) EditText mEditTextSessionName;
     @BindView(R.id.textViewStartTime) TextView mTextViewStartTime;
@@ -38,10 +44,23 @@ public class CreateSessionActivity extends AppCompatActivity implements AdapterV
     @BindView(R.id.editTextModule) EditText mEditTextModule;
     @BindView(R.id.editTextDescription) EditText mEditTextDescription;
 
+    private Calendar start_time;
+    private Calendar end_time;
+
+    private TextView activeDisplay;
+    private Calendar activeTime;
+    TimePickerDialog.OnTimeSetListener startTimeListener, endTimeListener;
+    static final int TIME_DIALOG_ID = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_session);
+
+        //we initialize both times
+        start_time = Calendar.getInstance();
+        end_time = Calendar.getInstance();
 
         Spinner spinner = findViewById(R.id.cohort_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cohorts, android.R.layout.simple_spinner_item);
@@ -57,6 +76,8 @@ public class CreateSessionActivity extends AppCompatActivity implements AdapterV
         mEditTextModule.setOnClickListener((View.OnClickListener) this);
         mEditTextDescription.setOnClickListener((View.OnClickListener) this);
         mTextViewDate.setOnClickListener((View.OnClickListener)this);
+        updateDisplay(mTextViewStartTime, start_time);
+        updateDisplay(mTextViewEndTime, end_time);
     }
 
     @Override
@@ -80,12 +101,12 @@ public class CreateSessionActivity extends AppCompatActivity implements AdapterV
 
     }
 
-    @SuppressLint("SetTextI18n")
-    @Override
-    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-        mTextViewEndTime.setText(hourOfDay + ":" + minute);
-        mTextViewEndTime.setText(hourOfDay + ":" + minute);
-    }
+//    @SuppressLint("SetTextI18n")
+//    @Override
+//    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+//
+//        mTextViewEndTime.setText(hourOfDay + ":" + minute);
+//    }
 
     @Override
     public void onClick(View view) {
@@ -99,18 +120,61 @@ public class CreateSessionActivity extends AppCompatActivity implements AdapterV
         if (view == mTextViewDate) {
             DialogFragment datePicker = new FragmentDate();
             datePicker.show(getSupportFragmentManager(), "date picker");
-
         }
 
         if (view == mTextViewStartTime) {
-            DialogFragment timePicker = new TimeFragment();
-            timePicker.show(getSupportFragmentManager(), "Start time picker");
+            showTimeDialog(mTextViewStartTime, start_time);
+
         }
 
         if (view == mTextViewEndTime) {
-            DialogFragment timePicker = new TimeFragment();
-            timePicker.show(getSupportFragmentManager(), "End time picker");
+            showTimeDialog(mTextViewEndTime, end_time);
         }
-
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case TIME_DIALOG_ID:
+                return new TimePickerDialog(this, timeSetListener, activeTime.get(Calendar.HOUR_OF_DAY), activeTime.get(Calendar.MINUTE), DateFormat.is24HourFormat(this));
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog) {
+        super.onPrepareDialog(id, dialog);
+        switch (id) {
+            case TIME_DIALOG_ID:
+                ((TimePickerDialog) dialog).updateTime(activeTime.get(Calendar.HOUR_OF_DAY),activeTime.get(Calendar.MINUTE));
+                break;
+        }
+    }
+
+    private void updateDisplay(TextView dateDisplay, Calendar time) {
+        dateDisplay.setText(String.format("%s:%s", time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE)));
+    }
+
+    private void showTimeDialog(TextView timeDisplay, Calendar time) {
+        activeDisplay = timeDisplay;
+        activeTime = time;
+        showDialog(TIME_DIALOG_ID);
+    }
+
+    private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+            activeTime.set(Calendar.HOUR_OF_DAY, hour);
+            activeTime.set(Calendar.MINUTE, minute);
+            updateDisplay(activeDisplay, activeTime);
+            unregisterTimeDisplay();
+        }
+    };
+
+    private void unregisterTimeDisplay() {
+        activeDisplay = null;
+        activeTime = null;
+    }
+
 }
