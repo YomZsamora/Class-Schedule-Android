440d6424586d9a05af922663ed7ab2b640161475
Gideon Okuro <gideonollonde@gmail.com>
2020-09-09 09:52:55 +0300

    display user name and email

--- app/src/main/java/com/moringa/class_schedule_app/ui/ProfileActivity.java
+++ app/src/main/java/com/moringa/class_schedule_app/ui/ProfileActivity.java
@@ -1,60 +1,132 @@
 package com.moringa.class_schedule_app.ui;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 import com.moringa.class_schedule_app.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
+    public static final String TAG = ProfileActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser user;
 
     @BindView(R.id.profileUserName)
     TextView mUsername;
     @BindView(R.id.profileEmail)
-    TextView mEmail;
+    TextView mProfileEmail;
+    @BindView(R.id.profilePicture)
+    ImageView mProfilePicture;
 
-    private FirebaseAuth auth;
-    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
 
-        authStateListener();
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+               user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+//                   String name = user.getDisplayName();
+//                   String email = user.getEmail();
+
+                    mUsername.setText(user.getDisplayName());
+                    mProfileEmail.setText(user.getEmail());
+
+//                boolean emailVerified = user.isEmailVerified();
+//
+//                String uid = user.getUid();
+
+                } else {
+
+                }
+            }
+
+        };
+//        displayUserProfile(user);
+    }
+
+
+    private void displayUserProfile(final FirebaseUser user) {
+        UserInfo getProfileInfo =  new UserInfo() {
+            @NonNull
+            @Override
+            public String getUid() {
+                return user.getUid();
+            }
+
+            @NonNull
+            @Override
+            public String getProviderId() {
+                return null;
+            }
+
+            @Nullable
+            @Override
+            public String getDisplayName() {
+                return user.getDisplayName();
+            }
+
+            @Nullable
+            @Override
+            public Uri getPhotoUrl() {
+                return null;
+            }
+
+            @Nullable
+            @Override
+            public String getEmail() {
+                return user.getEmail();
+            }
+
+            @Nullable
+            @Override
+            public String getPhoneNumber() {
+                return null;
+            }
+
+            @Override
+            public boolean isEmailVerified() {
+                return false;
+            }
+        };
+//        mAuth.getCurrentUser();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        auth.addAuthStateListener(authStateListener);
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        if (authStateListener != null) {
-            auth.removeAuthStateListener(authStateListener);
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 
-    private void authStateListener() {
-        auth  = FirebaseAuth.getInstance();
-        authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-//                    mUsername.setText(user.getDisplayName());
-//                    mEmail.setText(user.getEmail());
-                }
-            }
-        };
-    }
 }
+
+
+
+
+
